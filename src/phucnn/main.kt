9c4e3648c8e58f49a9a7e7d26b79376ac0c8272d
Nguyen Nhu Phuc <nguoiviet95@gmail.com>
2017-06-04 09:28:39 +0700

    first kotlin project

---
+++ src/phucnn/main.kt
@@ -0,0 +1,15 @@
+package phucnn
+
+import java.util.*
+
+/**
+ * Created by phucnn on 5/20/17.
+ */
+fun sum(a: Int, b: Int): Int {
+    return a + b
+}
+
+fun main(args: Array<String>) {
+    println("Hello \nWorld")
+    println(sum(5, 7))
+}
